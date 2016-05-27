6ae3bd3eecd9f43596c4ceef585ad858ad526fa8
Vitalii Solianyk <vitalii.solianyk@trinetix.com>
2016-05-27 13:45:08 +0300

    Add all needed files

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+@Component({
+    selector: 'my-app',
+    directives: [], 
+    template: `
+        <div>
+            <h3>Hello Angular 2 App</h3>
+        </div>
+    `
+})
+
+export class AppComponent {}
