804c7f8214ff814bc391d7abd773344c1a1c8925
Vitalii Solianyk <vitalii.solianyk@trinetix.com>
2016-06-30 12:48:55 +0300

    Update to RC3

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core'
+import { Component }            from '@angular/core'
+import { ROUTER_DIRECTIVES }    from '@angular/router'
 
 @Component({
     selector: 'my-app',
     directives: [], 
+    providers: [],
     template: `
         <div>
             <h3>Hello Angular 2 App</h3>
